67ec1557ea9f0a748701be97c0d973f43a5670f3
rafaelmelim <157408484+rafaelmelim@users.noreply.github.com>
2025-05-20 23:41:56 -0300

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@headlessui/react', '@heroicons/react'],
+        },
+      },
+    },
+  },
 });
