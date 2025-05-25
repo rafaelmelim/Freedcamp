5c963d533da5e0042b74b9a0601448f9fdf64b87
rafaelmelim <157408484+rafaelmelim@users.noreply.github.com>
2025-05-25 17:16:33 -0300

    Updated AdminPage.tsx

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -18,14 +18,14 @@ export function AdminPage() {
                 className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-100 rounded-md"
               >
                 <HomeIcon className="w-5 h-5" />
-                <span>My Board</span>
+                <span>Página Inicial</span>
               </Link>
               <Link
                 to="/archived"
                 className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-100 rounded-md"
               >
                 <ArchiveBoxIcon className="w-5 h-5" />
-                <span>Archived</span>
+                <span>Arquivos</span>
               </Link>
             </div>
             <div className="pt-4 mt-4 border-t border-gray-200">
@@ -35,7 +35,7 @@ export function AdminPage() {
                   className="flex items-center space-x-2 px-4 py-2 bg-gray-100 text-gray-900 rounded-md"
                 >
                   <Cog6ToothIcon className="w-5 h-5" />
-                  <span>Settings</span>
+                  <span>Configurações</span>
                 </Link>
                 <div className="mt-2 pl-4 space-y-2">
                   <Link
